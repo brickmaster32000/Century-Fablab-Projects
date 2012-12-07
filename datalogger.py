ec75bef8019a66cb50c5fc662051ef7bc01c75be
Christopher Pepin <brickmaster32000@gmail.com>
2012-12-07 15:38:02 -0600

    Datalogger exits cleanly on ctrl-c

--- datalogger.py
+++ datalogger.py
@@ -1,7 +1,14 @@
-import serial
-ser = serial.Serial('COM4', 9600, timeout=1)
-f=open("C:\\Users\\voge0017\\Desktop\\LOG.txt","a")
-f.write("*******************************************************")
+import serial, signal
+
+def handler(i,j):
+    ser.close()
+    f.close()
+    print "Closed serial port and file, have a good day"
+
+signal.signal(signal.SIGINT, handler)
+ser = serial.Serial('COM6', 9600, timeout=1)
+f=open("C:\\Users\\johnrupert\\Desktop\\LOG.txt","a")
+f.write("*******************************************************\n")
 while 1: 
     s = ser.readline(90)        
     f.write(s)
