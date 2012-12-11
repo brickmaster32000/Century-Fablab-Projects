ee11d66ef45d7ae0a827a01d3a847542ba4a6b83
killaimdie <jacobvoges@gmail.com>
2012-12-11 10:46:37 -0600

    improved the way it lists it kind of

--- parser.py
+++ parser.py
@@ -1,31 +1,17 @@
-
 d = open("C:\\Users\\voge0017\\Desktop\\LOG12_8_2012.txt","r")
-
 a = open("C:\\Users\\voge0017\\Desktop\\axl.txt","w")
-
 b = open("C:\\Users\\voge0017\\Desktop\\bar.txt","w")
+lines = []
 
-while True:
-    text = d.readline()
-    if 'Time' in text:
-        a.write(text)
-        b.write(text)
-    if 'X1' in text:
-        a.write(text)
-    if 'X2' in text:
-        a.write(text)
-    if 'Y2' in text:
-        a.write(text)
-    if 'Z2' in text:
-        a.write(text)
-    if 'Pressure' in text:
-        b.write(text)
-    if 'Temperature' in text:
-        b.write(text)
-    if 'Altitude' in text:
-        b.write(text)
+with d:
+    data = d.readlines() #create a list from the input file, each index in list is one line from file
+for i in range(len(data)):
+    lines.append(data[i].rstrip('\n').split(","))
+
+print lines
 
 d.close()
 a.close()
 b.close()
 print "I'm done."
+
