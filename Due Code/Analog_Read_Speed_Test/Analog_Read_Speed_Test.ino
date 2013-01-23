d098390a6aaaf31bf7add4bb7092df720feec402
Christopher Pepin <brickmaster32000@gmail.com>
2013-01-23 16:14:27 -0600

    Tested accelerometer speed

---
+++ Due Code/Analog_Read_Speed_Test/Analog_Read_Speed_Test.ino
@@ -0,0 +1,35 @@
+int xA = A2;
+int yA = A3;
+int zA = A4;
+
+int xB = A5;
+int yB = A6;
+int zB = A7;
+
+
+
+
+void setup(){
+  Serial.begin(115200);  
+}
+
+void loop(){
+  Serial.println(millis());
+  Serial.print("xA, ");Serial.println(analogRead(xA));
+  
+  Serial.println(millis());
+  Serial.print("yA, ");Serial.println(analogRead(yA));
+  
+  Serial.println(millis());
+  Serial.print("zA, ");Serial.println(analogRead(zA));
+  
+  Serial.println(millis());
+  Serial.print("xB, ");Serial.println(analogRead(xB));
+  
+  Serial.println(millis());
+  Serial.print("yB, ");Serial.println(analogRead(yB));
+  
+  Serial.println(millis());
+  Serial.print("zB, ");Serial.println(analogRead(zB));
+    
+}
