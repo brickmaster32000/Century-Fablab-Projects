7a174b82449598eaabb2f7d1eae7391265e6503d
Christopher Pepin <brickmaster32000@gmail.com>
2013-01-25 15:11:51 -0600

    changed formatting of accel output(jake)

--- Due Code/Analog_Read_Speed_Test/Analog_Read_Speed_Test.ino
+++ Due Code/Analog_Read_Speed_Test/Analog_Read_Speed_Test.ino
@@ -1,3 +1,8 @@
+#include <Adafruit_BMP085.h>
+#include <Wire.h>
+
+Adafruit_BMP085 bmp;
+
 int xA = A2;
 int yA = A3;
 int zA = A4;
@@ -6,30 +11,20 @@ int xB = A5;
 int yB = A6;
 int zB = A7;
 
-
-
-
 void setup(){
   Serial.begin(115200);  
 }
 
 void loop(){
-  Serial.println(millis());
-  Serial.print("xA, ");Serial.println(analogRead(xA));
   
-  Serial.println(millis());
-  Serial.print("yA, ");Serial.println(analogRead(yA));
+ 
+    Serial.print(millis());
+    Serial.print(",xA,");Serial.print(analogRead(xA));
+    Serial.print(",yA,");Serial.print(analogRead(yA));
+    Serial.print(",zA,");Serial.print(analogRead(zA));
+    Serial.print(millis());Serial.print(",");
+    Serial.print(",xB,");Serial.print(analogRead(xB));
+    Serial.print(",yB,");Serial.print(analogRead(yB));
+    Serial.print(",zB,");Serial.println(analogRead(zB));
   
-  Serial.println(millis());
-  Serial.print("zA, ");Serial.println(analogRead(zA));
-  
-  Serial.println(millis());
-  Serial.print("xB, ");Serial.println(analogRead(xB));
-  
-  Serial.println(millis());
-  Serial.print("yB, ");Serial.println(analogRead(yB));
-  
-  Serial.println(millis());
-  Serial.print("zB, ");Serial.println(analogRead(zB));
-    
 }
