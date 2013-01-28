ec174dda6f5b9f759027e50386a7664356b2ec88
Christopher Pepin <brickmaster32000@gmail.com>
2013-01-28 14:36:58 -0600

    updated bmp library, created baseline sensor due code
    updated bmp library, created baseline sensor due code

---
+++ Due Code/Due_Sensor_Code/Due_Sensor_Code.ino
@@ -0,0 +1,64 @@
+#include <Adafruit_BMP085.h>
+#include <Wire.h>
+
+Adafruit_BMP085 bmp;
+
+// the ratios are not final, they need to be tuned to a realistic value
+const int baromToGPS = 2
+const int accelToBaromRatio = 70; // maybe half a second based on the 
+
+const int ACTIVATION = arbitrary value;
+
+boolean brake = false;
+
+int xA = A2;
+int yA = A3;
+int zA = A4;
+
+int xB = A5;
+int yB = A6;
+int zB = A7;
+
+int proximity = A8;
+int brakeDist = proximity;
+
+void setup(){
+  Serial.begin(115200);
+  bmp.begin(); 
+}
+
+void loop(){
+  
+                //1, 2  ,3, 4,5, 6,7, 8,9, 10  ,11,12,13,14,15,16,17,   18    ,     19    ,   20     ,    21  ,   22    ,
+  Serial.println(",TimeA, ,xA, ,yA, ,zA, ,TimeB,  ,xB,  ,yB,  ,zB,  ,TimeBrake, BrakePosit, Activated, TimeBar, Altitude,TimeGPS, Latitude,, Longitude,,Quality,SAT#,,AltitudeGPS,,,,"); // Include headers at the top of the file for csv ease
+
+  for(int i = 0; i < baromToGPS; i++);{
+    //Print the barometer output
+    Serial.print(",,,,,,,,,,,,,,,,,");Serial.print(millis()); // place the beginning of the barometer readout in it's place, 8 cells to the right
+    Serial.print(",");Serial.println(bmp.readAltitude());
+  
+    //Print accelerometer data and drag brake when not printing any other sensors
+    for(int j = 0; j < accelToBAromRatio; j++){
+      
+      Serial.print(",TimeA,");Serial.print(millis());
+      Serial.print(",xA,");Serial.print(analogRead(xA));
+      Serial.print(",yA,");Serial.print(analogRead(yA));
+      Serial.print(",zA,");Serial.print(analogRead(zA));
+      Serial.print(",TimeB,");Serial.print(millis());
+      Serial.print(",xB,");Serial.print(analogRead(xB));
+      Serial.print(",yB,");Serial.print(analogRead(yB));
+      Serial.print(",zB,");Serial.print(analogRead(zB));
+      
+      brakeDist = analogRead(proximity);
+      Serial.print(",,,,,,,,,,,,,,,,,");Serial.print(millis());
+      Serial.print(",");Serial.println(brakeDist);
+      
+      if(brakeDist > ACTIVATION){
+        brake=true;
+        Serial.print(",,,,,,,,,,,,,,,,,");Serial.print(millis());
+        Serial.print(",");Serial.print(brakeDist);
+        Serial.print(",");Serial.println("Activated");
+      }
+    }
+  }
+}
