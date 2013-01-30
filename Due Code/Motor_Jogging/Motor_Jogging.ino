5a86e86f53f3e2773db73cee02655a19042e7afd
Christopher Pepin <brickmaster32000@gmail.com>
2013-01-30 15:51:06 -0600

    Added due code to run motor

---
+++ Due Code/Motor_Jogging/Motor_Jogging.ino
@@ -0,0 +1,35 @@
+int const ledPin = 7;
+int const motorControl = 6;
+int const ledButton = 5;
+int const motorButton = 4;
+
+boolean armed = false;
+
+void setup(){
+  pinMode(ledPin, OUTPUT);
+  pinMode(motorControl, OUTPUT);
+  
+  pinMode(ledButton, INPUT);
+  pinMode(motorButton, INPUT);
+  
+  digitalWrite(ledPin, LOW);
+  digitalWrite(motorControl, LOW);
+}
+
+void loop(){
+  while(!digitalRead(ledButton) && !armed){
+    if(digitalRead(motorButton)){
+      analogWrite(motorControl, 255); //Need to start motor at full power to start spinning
+      delay(10);
+      analogWrite(motorControl, 50);
+      delay(100);
+    }else{
+      analogWrite(motorControl, 0);
+    }
+  }
+  armed = true;
+  digitalWrite(ledPin, HIGH);
+  
+  delay(100);
+    
+}
