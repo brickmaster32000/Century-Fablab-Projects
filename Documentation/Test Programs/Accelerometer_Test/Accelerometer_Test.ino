a09e412675f4206d1dfe5f8b3c7c2a903a6d3085
Christopher Pepin <brickmaster32000@gmail.com>
2013-02-15 15:19:42 -0600

    More accelerometer data

--- Documentation/Test Programs/Accelerometer_Test/Accelerometer_Test.ino
+++ Documentation/Test Programs/Accelerometer_Test/Accelerometer_Test.ino
@@ -4,13 +4,18 @@ void setup() {
   analogReadResolution(12);
   Serial.begin(9600);
   
-  for(int i = 0; i < 10; i++){
+  
+  delay(3000);
+  Serial.println("Begin");
+  delay(1000);
+  for(int i = 0; i < 500; i++){
     Serial.print("x1,");Serial.print(analogRead(A2));
     Serial.print(",y1,");Serial.print(analogRead(A3));
     Serial.print(",z1,");Serial.print(analogRead(A4));
     Serial.print(",x2,");Serial.print(analogRead(A5));
     Serial.print(",y2,");Serial.print(analogRead(A6));
-    Serial.print(",z2,");Serial.println(analogRead(A7));
+    Serial.print(",z2,");Serial.print(analogRead(A7));Serial.print('\n');
+    
   }
   
 
