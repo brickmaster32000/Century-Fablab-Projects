05685288e768ffd10a26f16dda444e4c78528997
Christopher Pepin <brickmaster32000@gmail.com>
2013-04-01 15:08:00 -0500

    Updated constants

--- Due Code/Due_Sensor_Code/Full_Scale_Launch_2/Full_Scale_Launch_2.ino
+++ Due Code/Due_Sensor_Code/Full_Scale_Launch_2/Full_Scale_Launch_2.ino
@@ -8,16 +8,26 @@ void barometerRead(int offset);
 void GPSRead(int offset);
 
 double findApogee(double v0, double y0);
+void airbrakeCheck();
 
-double vt; //terminal velocity
-double g; //gravitiational acceleration
-double targetHeight;
+double vt = 475.88; //terminal velocity in ft/sec
+                    //assumes
+                    //m = 1.5528 slugs
+                    //g = 32.2 ft/s^2
+                    //Cd = 0.55
+                    //A = 0.349 ft^2
+                    //p = 0.0023
+double g = 32.2; //gravitiational acceleration
+double meterToFeet = 3.28084;
+double targetHeight = 4600; //in feet
+long index=0;
 GPS gps(&Serial1);
 Adafruit_BMP085 bmp;
 
 // the ratios are not final, they need to be tuned to a realistic value
 const int accelToBaromRatio = 10; // maybe half a second based on data received. 
 boolean armed = false;
+
 
 int xA = A2;
 int yA = A3;
@@ -41,7 +51,7 @@ void setup(){
   
   pinMode(motorButton, INPUT);
   
-  //When the arduino is truned on holding the the motor button
+  //When the arduino is turned on holding the the motor button
   //for more than 2 seconds arms it
   if(digitalRead(motorButton)){
     int startTime = millis();
@@ -64,12 +74,51 @@ void setup(){
     }  
   } 
   
-                //  1  ,2,  3,4,  5,6,  7,8,     9   ,10,    11    ,12,   13  ,14,   15,  ,16 
-  Serial.println("TimeB,  ,xB,  ,yB,  ,zB, ,TimeBrake,  ,BrakePosit,  ,TimeBar,  ,Altitude,  ");
+
 }
 
 void loop(){
-  //Gets two seperate
+  
+  airbrakeCheck();
+}
+
+/******************************************************************************
+Takes a set of accelerometer readings and prints them
+offset is used to add commas at the begining of the 
+string to position it in the proper collumn
+******************************************************************************/
+void accelerometerRead(int offset){
+  for(int i = 0; i < offset; i++){
+    Serial.print(',');
+  }
+  Serial.print(index++);
+  Serial.print("TimeB,");Serial.print(millis());
+  Serial.print(",xB,");Serial.print(analogRead(xB));
+  Serial.print(",BrakePosit,");Serial.println(analogRead(proximity));
+}
+
+void barometerRead(int offset){
+  for(int i = 0; i < offset; i++){
+    Serial.print(',');
+  }
+  Serial.print(index++);
+  Serial.print("TimeBar,");Serial.print(millis());
+  Serial.print(",Altitude,");Serial.println(bmp.readAltitude());
+}
+
+
+/************************************************************************
+Calculates apogee using initial velocity and altitude
+************************************************************************/
+double findApogee(double v0, double y0){
+  return y0 + (pow(vt, 2)/(2 * g))* log((pow(v0, 2)+pow(vt, 2))/pow(vt,2));
+}
+
+/************************************************************************
+Gathers neccessary data to predict apogee and deploys airbrake if neccessary
+************************************************************************/
+void airbrakeCheck(){
+  //Gets two seperate altitudes by averaging small samples.
   float altitude = 0;
   long time[4]; //holds beginning and end time of altitude samples
   
@@ -77,7 +126,7 @@ void loop(){
   for(int i = 0; i < 5; i++){
     altitude += bmp.readAltitude();
   }
-  float altitudeStart = altitude / 5;
+  float altitudeStart = (altitude / 5) * meterToFeet;
   time[1] = millis();
   
   delay(500);
@@ -87,10 +136,10 @@ void loop(){
   for(int i = 0; i < 5; i++){
     altitude += bmp.readAltitude();
   }
-  float altitudeEnd = altitude / 5;
+  float altitudeEnd = (altitude / 5)* meterToFeet;
   time[3] = millis();
   
-  double velocity = (altitudeEnd - altitudeStart)/(((time[3]-time[2])/2)-((time[1]-time[0])/2));
+  double velocity = (altitudeEnd - altitudeStart)/(((time[3]-time[2])/2)-((time[1]-time[0])/2))*1000; //Time is in millis so multiply by 1000 to get ft/sec
   double apogee = findApogee(velocity, altitudeEnd);
   
   if(apogee >= targetHeight){
@@ -98,36 +147,4 @@ void loop(){
     delay(1000);
     analogWrite(motorControl, 0);
   }
-  
-}
-
-/******************************************************************************
-Takes a set of accelerometer readings and prints them
-offset is used to add commas at the begining of the 
-string to position it in the proper collum
-******************************************************************************/
-void accelerometerRead(int offset){
-  for(int i = 0; i < offset; i++){
-    Serial.print(',');
-  }
-  
-  Serial.print("TimeB,");Serial.print(millis());
-  Serial.print(",xB,");Serial.print(analogRead(xB));
-  Serial.print(",yB,");Serial.print(analogRead(yB));
-  Serial.print(",zB,");Serial.print(analogRead(zB));
-  Serial.print(",BrakePosit,");Serial.println(analogRead(proximity));
-}
-
-void barometerRead(int offset){
-  for(int i = 0; i < offset; i++){
-    Serial.print(',');
-  }
-  
-  Serial.print("TimeBar,");Serial.print(millis());
-  Serial.print(",Altitude,");Serial.println(bmp.readAltitude());
-}
-  
-double findApogee(double v0, double y0){
-  return y0 + (pow(vt, 2)/(2 * g))* log10((pow(v0, 2)+pow(vt, 2))/pow(vt,2));
-}
-  
+}  
